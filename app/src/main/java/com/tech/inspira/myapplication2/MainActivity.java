8269c021d10756fb2a48089e6cf3fc48416a51c1
chesco134 <chesco134@gmail.com>
2016-02-10 17:38:23 -0600

    Agregados comentarios y ordenada la clase principal, aún faltan los ficheros xml

--- app/src/main/java/com/tech/inspira/myapplication2/MainActivity.java
+++ app/src/main/java/com/tech/inspira/myapplication2/MainActivity.java
@@ -9,97 +9,102 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
+/********************************************************************************
+ *
+ * La presente es una clase que encierra diferentes tipos de animación básica.
+ * Busca ejemplificar el uso de la clase Animation y el contenido del directorio
+ * de recursos "anim".
+ *
+ * ********************************************************************************/
+
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textoAnimacion;
-    private Animation mAnimation;
-    private Animation overshotAnim;
+    /** Zona de etiquetas en pantalla **/
+    private TextView textoMovimientoHorizontal;
     private TextView overshotText;
-    private Animation rotationAnimation;
-    private TextView rotationText;
-    private Animation dilatacionAnim;
+    private TextView textoGiratorio;
     private TextView textoDilatacion;
-    private Animation alfaAnim;
     private TextView textoConAlfa;
+
+    /** Zona de objetos de animación **/
+    // Cada objeto de animación representa un archivo en el directorio res/anim.
+
+    private Animation animacionDeMovimientoHorizontal;
+    private Animation overshotAnim;
+    private Animation animacionDeGiro;
+    private Animation animacionDeDilatacion;
+    private Animation animacionDeEfectoAlfa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.my_main);
-        textoAnimacion = (TextView)findViewById(R.id.my_main_texto_animacion);
-        rotationText = (TextView)findViewById(R.id.my_main_rotacion);
+        super.onCreate(savedInstanceState); // Ésta línea tiene que ir a fuerzas.
+        setContentView(R.layout.my_main); // Ésta línea dice cuál pantalla será mostrada.
+
+        /** A continuación sigue un proceso de inicialización de atributos **/
+        /** Primero se pasan los componentes visuales del xml al objeto java **/
+        textoMovimientoHorizontal = (TextView)findViewById(R.id.my_main_texto_animacion);
+        overshotText = (TextView) findViewById(R.id.my_main_overshot_text);
+        textoGiratorio = (TextView)findViewById(R.id.my_main_rotacion);
         textoDilatacion = (TextView) findViewById(R.id.my_main_dilatacion);
         textoConAlfa = (TextView) findViewById(R.id.my_main_alfa);
-        mAnimation = AnimationUtils.loadAnimation(this,R.anim.traslacion_derecha);
-        overshotAnim = AnimationUtils.loadAnimation(this,R.anim.animacion);
-        rotationAnimation = AnimationUtils.loadAnimation(this,R.anim.rotation);
-        dilatacionAnim = AnimationUtils.loadAnimation(this,R.anim.escala);
-        alfaAnim = AnimationUtils.loadAnimation(this,R.anim.alfa);
-        alfaAnim.setRepeatMode(Animation.RESTART);
-        alfaAnim.setRepeatCount(20);
-        textoConAlfa.startAnimation(alfaAnim);
-        textoConAlfa.setOnClickListener(new View.OnClickListener() {
+        /** Cada uno de los R.id.* debe existir dentro del xml de la pantalla a mostrar **/
+
+        /** A continuación se cargarán los archivos de animación del directorio res/anim **/
+        animacionDeMovimientoHorizontal = AnimationUtils.loadAnimation(this,R.anim.traslacion_derecha);
+        overshotAnim = AnimationUtils.loadAnimation(this,R.anim.overshot);
+        animacionDeGiro = AnimationUtils.loadAnimation(this,R.anim.rotation);
+        animacionDeDilatacion = AnimationUtils.loadAnimation(this,R.anim.dilatacion);
+        animacionDeEfectoAlfa = AnimationUtils.loadAnimation(this,R.anim.efecto_alfa);
+
+        /** Después debemos colocar ciertas banderas en los objetos de animación antes creados **/
+        animacionDeMovimientoHorizontal.setRepeatCount(Animation.RESTART); // Reinicia la animación.
+        animacionDeMovimientoHorizontal.setRepeatCount(20); // Se repetirá 20 veces.
+        //animacionDeMovimientoHorizontal.setFillAfter(true);
+        overshotAnim.setRepeatMode(Animation.RESTART);
+        overshotAnim.setRepeatCount(20);
+        animacionDeGiro.setRepeatMode(Animation.RESTART);
+        animacionDeGiro.setRepeatCount(20);
+        animacionDeDilatacion.setRepeatMode(Animation.RESTART);
+        animacionDeDilatacion.setRepeatCount(20);
+        animacionDeEfectoAlfa.setRepeatMode(Animation.RESTART);
+        animacionDeEfectoAlfa.setRepeatCount(20);
+
+        /** Posteriormente se redefinen algunos textos a algunas etiquetas de la pantalla **/
+        overshotText.append("\nRepeatMode: " + overshotAnim.getRepeatMode()
+                + "\nRepeatCount: " + overshotAnim.getRepeatCount());
+        textoMovimientoHorizontal.append("\nEl presente texto ha sido añadido");
+
+        /** Luego procedemos a iniciar cada una de las animaciones **/
+        textoMovimientoHorizontal.startAnimation(animacionDeMovimientoHorizontal);
+        overshotText.setAnimation(overshotAnim);
+        textoGiratorio.startAnimation(animacionDeGiro);
+        textoDilatacion.startAnimation(animacionDeDilatacion);
+        textoConAlfa.startAnimation(animacionDeEfectoAlfa);
+
+        /** Finalmente colocamos una acción a cada etiqueta responder a pulsaciones sobre ella **/
+        textoMovimientoHorizontal.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                v.startAnimation(alfaAnim);
+            public void onClick(View view) { // Simplemente reiniciamos la animación.
+                view.startAnimation(animacionDeMovimientoHorizontal);
             }
         });
-        dilatacionAnim.setRepeatMode(Animation.RESTART);
-        dilatacionAnim.setRepeatCount(20);
-        textoDilatacion.startAnimation(dilatacionAnim);
+        textoGiratorio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.startAnimation(animacionDeGiro);
+            }
+        });
         textoDilatacion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                v.startAnimation(dilatacionAnim);
+                v.startAnimation(animacionDeDilatacion);
             }
         });
-        rotationAnimation.setRepeatCount(20);
-        rotationAnimation.setRepeatMode(Animation.RESTART);
-        rotationText.setOnClickListener(new View.OnClickListener() {
+        textoConAlfa.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                v.startAnimation(rotationAnimation);
+                v.startAnimation(animacionDeEfectoAlfa);
             }
         });
-        rotationText.startAnimation(rotationAnimation);
-        overshotText = (TextView)findViewById(R.id.my_main_overshot_text);
-        //mAnimation.setFillAfter(true);
-        mAnimation.setRepeatCount(Animation.RESTART);
-        mAnimation.setRepeatCount(20);
-        textoAnimacion.startAnimation(mAnimation);
-        overshotAnim.setRepeatMode(Animation.RESTART);
-        overshotAnim.setRepeatCount(20);
-        overshotText.setAnimation(overshotAnim);
-        overshotText.append("\nRepeatMode: " + overshotAnim.getRepeatMode()
-                + "\nRepeatCount: " + overshotAnim.getRepeatCount());
-        textoAnimacion.append("\nEl presente texto ha sido añadido");
-        textoAnimacion.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                view.startAnimation(mAnimation);
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
